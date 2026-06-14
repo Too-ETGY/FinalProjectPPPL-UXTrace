e5dcadfd098a94acbcc38e6e663a3ff9c22bc2b8
Dzakiyaadila <dzakiyaadila4@gmail.com>
2026-06-14 10:11:59 +0700

    setup runner for automation testing

--- uxtrace-automation-testing/src/test/java/runner/TestRunner.java
+++ uxtrace-automation-testing/src/test/java/runner/TestRunner.java
@@ -1,1 +1,20 @@
 package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/02_alarm.feature", //tambah fiturnya masing2 di sini
+        glue = "defs",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/alarm-test-report.html",
+                "json:target/cucumber-reports/cucumber.json"
+        },
+        tags = "@Alarm", //tambah tag masing2 di sini
+        monochrome = true
+)
+public class TestRunner {
+}
