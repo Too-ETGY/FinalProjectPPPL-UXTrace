e3e2b4d623ec5810fe5da14a9419135a5c8d2dd3
Dzakiyaadila <dzakiyaadila4@gmail.com>
2026-06-14 10:17:15 +0700

    add BasePage as parent class for all page objects

--- uxtrace-automation-testing/src/test/java/pages/BasePage.java
+++ uxtrace-automation-testing/src/test/java/pages/BasePage.java
@@ -1,4 +1,18 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+//guys page object masing2 nge extend file BasePaeg ini yakk
+    protected WebDriver driver;
+
+    public BasePage() {
+        // Constructor kosong untuk subclass
+    }
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
