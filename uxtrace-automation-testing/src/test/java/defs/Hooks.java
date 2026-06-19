471524819f3f15116c6ae0c7690ced69ed645156
adell10 <ruaadelia@mail.ugm.ac.id>
2026-06-19 11:15:54 +0700

    fix: perbaiki hooks branch auth

--- uxtrace-automation-testing/src/test/java/defs/Hooks.java
+++ uxtrace-automation-testing/src/test/java/defs/Hooks.java
@@ -1,4 +1,26 @@
 package defs;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Hooks {
+
+    public static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
