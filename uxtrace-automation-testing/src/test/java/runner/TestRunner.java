c6734bb8d7cd805f8c980d8dcb3f7984e3e06aab
adell10 <ruaadelia@mail.ugm.ac.id>
2026-06-19 16:26:31 +0700

    fix: update test runner

--- uxtrace-automation-testing/src/test/java/runner/TestRunner.java
+++ uxtrace-automation-testing/src/test/java/runner/TestRunner.java
@@ -6,14 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/02_alarm.feature", //tambah fiturnya masing2 di sini
+        features = "src/test/resources/features/03_console.feature", //tambah fiturnya masing2 di sini
         glue = "defs",
         plugin = {
                 "pretty",
-                "html:target/cucumber-reports/alarm-test-report.html",
+                "html:target/cucumber-reports/console-test-report.html",
                 "json:target/cucumber-reports/cucumber.json"
         },
-        tags = "@Alarm", //tambah tag masing2 di sini
+        tags = "@Console", //tambah tag masing2 di sini
         monochrome = true
 )
 public class TestRunner {
